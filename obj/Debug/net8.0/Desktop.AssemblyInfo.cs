e0f4f2c61ebef7e7104f8d1929a50a3c880315cf
unknown <solisluris@gmail.com>
2024-11-14 15:26:30 -0600

    delete email

--- obj/Debug/net8.0/Desktop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Desktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f68113218de49e23746c1866412fcdce89297a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17274acef238b3cd960f9c0be952de72443a4a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
